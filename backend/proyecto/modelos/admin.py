342049f40a9270c236b75ed13e57be86d45a00a3
Luciano Fandiño <63745887+Lucianof51@users.noreply.github.com>
2021-08-15 19:32:33 -0300

    Add files via upload

---
+++ backend/proyecto/modelos/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from modelos.models import Locador, Propiedad, Inquilino, Garante, Proveedor, Contrato, Pago, Reporte, Cierre_de_caja, Aviso, Comprobante_electronico, ReportePrueba, Data
+
+admin.site.register(Locador)
+admin.site.register(Propiedad)
+admin.site.register(Inquilino)
+admin.site.register(Garante)
+admin.site.register(Proveedor)
+admin.site.register(Contrato)
+admin.site.register(Pago)
+admin.site.register(Reporte)
+admin.site.register(Cierre_de_caja)
+admin.site.register(Aviso)
+admin.site.register(Comprobante_electronico)
+admin.site.register(ReportePrueba)
+admin.site.register(Data)
