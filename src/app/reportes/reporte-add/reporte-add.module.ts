7286ebb4f510b0f413aae5af668769063b9f8ddd
Luciano Fandiño <63745887+Lucianof51@users.noreply.github.com>
2021-08-16 16:33:09 -0300

    Validación Agregada

--- src/app/reportes/reporte-add/reporte-add.module.ts
+++ src/app/reportes/reporte-add/reporte-add.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,8 +13,10 @@ import { ReporteAddPage } from './reporte-add.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    ReporteAddPageRoutingModule
+    ReporteAddPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [ReporteAddPage]
 })
 export class ReporteAddPageModule {}
+
